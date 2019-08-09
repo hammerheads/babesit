d927cb92d44fe5a31954635b37a9ac7e925496dc
Matheus <mattreisro@gmail.com>
2019-08-09 03:40:50 +0100

    WebApp core

---
+++ src/public/js/main.js
@@ -0,0 +1,91 @@
+const API = 'http://localhost:3000';
+
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+
+function startPage(customer) {
+    $('#Profile-Avatar').attr({ src: customer.pic });
+    $('#username').html(customer.firstName);
+}
+
+
+
+$( document ).ready(function () {
+   $.ajax({
+        url: API + '/customer/1',
+        type: 'GET',
+        success: function (res) {
+            startPage(res);
+        }
+    })
+    tempBabe()
+})
+
+
+function submitBabe() {
+
+    $("#cid").val("1");    
+
+    $.ajax({
+        url: API + '/customer/' + $("#cid").val() + "/babe/",
+        type: 'POST',
+        data: JSON.stringify({
+            firstName: $("#firstName").val(),
+            lastName: $("#lastName").val(),
+            email: $("#email").val(),
+            phone: $("#phone").val(),
+            address: $("#address").val(),
+            locations: [$("#locations1").val(), $("#locations2").val(), $("#locations3").val()]
+        }),
+        async: true,
+        contentType: "application/json",
+        success: function() {
+            console.log("success");
+        },
+        error: function(status, error) {
+            console.log('Something went wrong: ', status, error);
+        }
+    })
+
+}
+
+
+function tempBabe() {
+    $.ajax({
+        url: '/babe-add/',
+        type: 'GET',
+        success: function (res) {
+            $('#TOCHANGE > div').remove()
+            $('#TOCHANGE').append(res)
+            $('#addBabes').click(function () {
+                tempFormBabe()
+            })
+        }
+    })
+}
+
+
+function tempFormBabe() {
+    $.ajax({
+        url: '/babe-form/',
+        type: 'GET',
+        success: function (res) {
+            $('#TOCHANGE > div').remove()
+            $('#TOCHANGE').append(res)
+        }
+    })
+}
+
+$('#babes-show').click(function () {
+    $.ajax({
+        url: '/babe-profile/',
+        type: 'GET',
+        success: function (res) {
+            $('#TOCHANGE > div').remove()
+            $('#TOCHANGE').append(res)
+        }
+    })
+})
